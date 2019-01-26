0a1449e6f1fd9f0fe2dd7a518be7f61584b313c6
Kryxzael <kasper.ramskjell98@gmail.com>
2019-01-26 23:43:06 +0100

    Make junk fade in when spawning

---
+++ Assets/TwitterVsReddit/Objects/Junk/FadeInSprite.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+/// <summary>
+/// Makes a sprite fade in when it spawns
+/// </summary>
+[RequireComponent(typeof(SpriteRenderer))]
+public class FadeInSprite : MonoBehaviour
+{
+    [Description("The time it takes for the sprite to fade in (in seconds (eh. don't count on it))")]
+    public float FadeInTime = 1f;
+
+    private IEnumerator Start()
+    {        
+        SpriteRenderer spr = GetComponent<SpriteRenderer>();
+
+        for (float i = 0; i < 1f; i += 0.01f)
+        {
+            spr.color = new Color(1f, 1f, 1f, i);
+
+            yield return new WaitForSeconds(FadeInTime / 1000f);
+        }
+    }
+}
