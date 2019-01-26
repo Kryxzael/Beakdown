2231075602889d05c9dd3a4f84f208e12af81a7f
Kryxzael <kasper.ramskjell98@gmail.com>
2019-01-26 12:51:08 +0100

    Add junk class

---
+++ Assets/TwitterVsReddit/Objects/Junk/Junk.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Junk : MonoBehaviour
+{
+    [Header("Stats")]
+    [Description("The value in points this piece of junk will provide")]
+    public int Value;
+
+    [Description("Multiplier applied to the player's speed when the object is being held")]
+    public float SpeedMultipler;
+}
