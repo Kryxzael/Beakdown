5ca7a1ac2b444f0e235794f3529897d2abb2cc38
Kryxzael <kasper.ramskjell98@gmail.com>
2019-01-26 13:51:57 +0100

    Add 'Level' script with time limit

---
+++ Assets/TwitterVsReddit/Objects/GameManager/Level.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+/// <summary>
+/// Represents the main level manager. It includes among other things, the timer
+/// </summary>
+public class Level : MonoBehaviour
+{
+    [Header("Time limit")]
+    [Description("The time limit of the game")]
+    public int GameTimeSeconds;
+
+    /// <summary>
+    /// How much time is left on the clock
+    /// </summary>
+    public TimeSpan TimeLeft { get; set; }
+
+    /// <summary>
+    /// Is a game currently in progress
+    /// </summary>
+    public bool GameRunning { get; private set; }
+
+    /// <summary>
+    /// Begins the game
+    /// </summary>
+    public void StartGame()
+    {
+        TimeLeft = new TimeSpan(0, 0, GameTimeSeconds);
+        GameRunning = true;
+        StartCoroutine(CoTickTime());
+    }
+
+    /// <summary>
+    /// Ends the game and announces the winner.
+    /// </summary>
+    public void EndGame()
+    {
+        GameRunning = false;
+    }
+
+    private IEnumerator CoTickTime()
+    {
+        //Ticks the time until it reaches zero seconds
+        while (TimeLeft > new TimeSpan(0, 0, 0))
+        {
+            TimeLeft -= new TimeSpan(0, 0, seconds: 1);
+            yield return new WaitForSeconds(1);
+        }
+
+        //Ends the game
+        EndGame();
+    }
+
+    private void Start()
+    {
+        StartGame();
+    }
+
+    private void Update()
+    {
+        DebugScreenDrawer.Enable("time", "Time: " + TimeLeft);
+        DebugScreenDrawer.Enable("run", "InGame: " + GameRunning);
+    }
+
+}
