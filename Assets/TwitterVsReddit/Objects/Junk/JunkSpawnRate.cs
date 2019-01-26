2040237fb097f0733836cce8149d8069c811f408
Kryxzael <kasper.ramskjell98@gmail.com>
2019-01-26 22:15:05 +0100

    Add JunkSpawnRate scriptable object

---
+++ Assets/TwitterVsReddit/Objects/Junk/JunkSpawnRate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+/// <summary>
+/// Scriptable object that represents the spawn rate of items
+/// </summary>
+[CreateAssetMenu(menuName = "Junk Spawn Rate Info")]
+public class JunkSpawnRate : ScriptableObject
+{
+    [Description("The chance of this item being spawned")]
+    [Range(0f, 1f)]
+    public float SpawnChance;
+
+    [Description("The item that will be spawned")]
+    public Junk Item;
+}
