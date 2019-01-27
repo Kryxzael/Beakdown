2e19d037c90a7fae89335b136156761dc0e33058
Kryxzael <kasper.ramskjell98@gmail.com>
2019-01-27 11:43:13 +0100

    Implement main menu

---
+++ Assets/TwitterVsReddit/Objects/GameManager/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(UnityEngine.Random.Range(1, SceneManager.sceneCount));
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
